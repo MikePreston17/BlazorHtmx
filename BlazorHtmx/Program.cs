ff22a477976a75f2e4b94c4991f55a30f1b65034
mikepreston17 <michael.n.preston@gmail.com>
2024-02-13 00:57:47 -0600

    ++ CodeMechanic packages

--- BlazorHtmx/Program.cs
+++ BlazorHtmx/Program.cs
@@ -1,9 +1,12 @@
 using BlazorHtmx.Components;
 using BlazorHtmx.Components.Shared;
+using CodeMechanic.FileSystem;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+DotEnv.Load();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
